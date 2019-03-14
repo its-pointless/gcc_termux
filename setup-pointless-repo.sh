af6c0ed6d3bc5177f7f4ab7cbd1ab94b66c375dc
its-pointless <bored@goat.si>
2019-03-14 21:13:49 +1100

    fix that

--- setup-pointless-repo.sh
+++ setup-pointless-repo.sh
@@ -6,7 +6,7 @@ apt-get  --assume-yes install coreutils gnupg apt-transport-https wget
 # Make the sources.list.d directory
 mkdir $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
-echo "deb [trusted=yes] https://its-pointless.github.io/files/ termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
+echo "deb https://its-pointless.github.io/files/ termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 # Download signing key from https://its-pointless.github.io/pointless.gpg 
 wget https://its-pointless.github.io/pointless.gpg
 apt-key add pointless.gpg
