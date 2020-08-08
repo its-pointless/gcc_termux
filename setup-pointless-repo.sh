0cbb4bd5c4e5af77d5015f1db1925a252eda25cc
its-pointless <bored@goat.si>
2020-08-08 11:54:55 +1000

    fix scripts

--- setup-pointless-repo.sh
+++ setup-pointless-repo.sh
@@ -1,19 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/sh
 # Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
 apt-get update
-apt-get  --assume-yes upgrade 
-apt-get  --assume-yes install coreutils gnupg wget 
+apt-get --assume-yes upgrade
+apt-get --assume-yes install coreutils gnupg
 # Make the sources.list.d directory
 mkdir -p $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
-if apt-cache policy | grep -q "https://termux.org/packages" ; then
+if apt-cache policy | grep -q "termux.*24\|termux.org\|bintray.*24" ; then
 echo "deb https://its-pointless.github.io/files/24 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 else
 echo "deb https://its-pointless.github.io/files/21 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 fi
-# Download signing key from https://its-pointless.github.io/pointless.gpg 
-wget https://its-pointless.github.io/pointless.gpg
-apt-key add pointless.gpg
-rm pointless.gpg
+# Add signing key from https://its-pointless.github.io/pointless.gpg
+if [ -n $(command -v curl) ]; then
+curl -sL https://its-pointless.github.io/pointless.gpg | apt-key add -
+elif [ -n $(command -v wget) ]; then
+wget -qO - https://its-pointless.github.io/pointless.gpg | apt-key add -
+fi
 # Update apt
 apt update
